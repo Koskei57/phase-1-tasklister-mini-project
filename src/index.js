29d11a99c0b58aa34d820db5e17f2707c9d8d428
Koskei57 <dennis.koskei@student.moringaschool.com>
2022-06-13 16:53:44 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     // your code here
-    let form = document.querySelector("form").addEventListener('submit', (e) => {
+    let form = document.querySelector("form")
+    form.addEventListener('submit', (e) => {
         e.preventDefault()
         handleToDo(e.target.new_task_description.value)
         form.reset()
